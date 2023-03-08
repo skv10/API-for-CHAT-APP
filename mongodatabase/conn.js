bd9fe4923bfc28d9a8e0ec9103d34fc8c5231d25
Sameer Shukla <42176715+skv10@users.noreply.github.com>
2023-03-08 10:34:44 +0000

    few development change

--- mongodatabase/conn.js
+++ mongodatabase/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     try {
-        const connn = await mongoose.connect("mongodb://localhost:27017/ChatApp");
+        const connn = await mongoose.connect(process.env.MONGO_URL);
         
         console.log('Mongodb is Connected');
         
--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
- console.log(`server is running at :${5000}`);
+ console.log(`server is running at :${PORT}`);
 });
